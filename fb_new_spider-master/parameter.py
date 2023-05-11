fe0e252afe627d3f9c3555360d611d425ade992b
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-11 17:50:04 +0530

    commited

---
+++ fb_new_spider-master/parameter.py
@@ -0,0 +1,7 @@
+#Facebook Credentials
+Facebook_username = 'yudhishthirsharma6@gmail.com'
+Facebook_password = 'Changepwd@1997'
+#url from which need to scrape data
+URL= "https://www.facebook.com/groups/1225966920763001"
+#Enter the relative path of your chromedriver
+relative_path = "./chromedriver.exe"
